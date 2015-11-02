0c5e5b5e95678ec3bfe2c89f4a4e85d3c986a131
Eashan Sahai <eashansahai@gmail.com>
2015-11-01 20:21:00 -0700

    Initial commit of the new RAPTACON website.

---
+++ js/controller/app.js
@@ -0,0 +1,67 @@
+var RaptaconApp = angular.module('RaptaconApp', [
+  'ngRoute',
+  'BlogFeedDirective'
+]);
+
+RaptaconApp.controller('RaptaconController', function ($scope) {
+  $scope.lastItem = "homeMenu";
+  $scope.go = function(menuID) {
+    $('#' + menuID).addClass('active');
+    if ( $scope.lastItem !== menuID) {
+      $('#' + $scope.lastItem).removeClass('active');
+    }
+    $scope.lastItem = menuID;
+  }
+});
+
+RaptaconApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: '/partials/home.html',
+        controller: 'RaptaconController'
+      }).
+      when('/blog', {
+        templateUrl: '/partials/blog.html',
+        controller: 'RaptaconController'
+      }).
+      when('/calendar', {
+        templateUrl: '/partials/calendar.html',
+        controller: 'RaptaconController'
+      }).
+      when('/contact', {
+        templateUrl: '/partials/contact.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage', {
+        templateUrl: '/partials/scrimmage.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage/video', {
+        templateUrl: '/partials/scrimmage/video.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage/gameRules', {
+        templateUrl: '/partials/scrimmage/gameRules.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage/questions', {
+        templateUrl: '/partials/scrimmage/questions.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage/answers', {
+        templateUrl: '/partials/scrimmage/answers.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage/rsvp', {
+        templateUrl: '/partials/scrimmage/rsvp.html',
+        controller: 'RaptaconController'
+      }).
+      when('/scrimmage/updates', {
+        templateUrl: '/partials/scrimmage/updates.html',
+        controller: 'RaptaconController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ js/controller/blogFeed.js
@@ -0,0 +1,22 @@
+(function() {
+  angular.module('BlogFeedDirective', [])
+    .controller('BlogFeedController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+      $scope.posts = [];
+      $http.get('http://raptacon.org/?json=get_recent_posts')
+      .then(success);
+
+      function success(response) {
+        $scope.posts = response.data.posts;
+      }
+
+      $scope.renderHtml = function (htmlCode) {
+            return $sce.trustAsHtml(htmlCode);
+      };
+    }])
+    .directive('blogFeed', function() {
+      return {
+        restrict: 'E',
+        templateUrl: '/partials/blogFeed.html'
+      };
+    });
+})();
